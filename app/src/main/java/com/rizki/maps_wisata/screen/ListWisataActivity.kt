dbb66388160861873da84523c2b458ab5fe364c4
Rizki Pm <udarizki2020@gmail.com>
2024-11-01 11:38:50 +0800

    update

---
+++ app/src/main/java/com/rizki/maps_wisata/screen/ListWisataActivity.kt
@@ -0,0 +1,21 @@
+package com.rizki.maps_wisata.screen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.rizki.maps_wisata.R
+
+class ListWisataActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_list_wisata)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
